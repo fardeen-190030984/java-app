9758995a54afc9a8e24ddfa133a589c103e22acc
Mahammad Fardeen Baig <99198089+fardeen-190030984@users.noreply.github.com>
2022-04-12 12:54:20 +0530

    Update add.java

--- add.java
+++ add.java
@@ -1,18 +1,10 @@
-import java.util.Scanner;
+
 import java.util.*;
 public class AddTwoNumbers2 {
 
     public static void main(String[] args) {
         
-        int num1, num2, sum;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter First Number: ");
-        num1 = sc.nextInt();
-        
-        System.out.println("Enter Second Number: ");
-        num2 = sc.nextInt();
-        
-        sc.close();
+        int num1=4, num2=9, sum;
         sum = num1 + num2;
         System.out.println("Sum of these numbers: "+sum);
     }
