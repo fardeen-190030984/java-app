fea2a6c1e51251ea45889eaee2e5a46455d43621
Mahammad Fardeen Baig <99198089+fardeen-190030984@users.noreply.github.com>
2022-04-12 12:58:14 +0530

    Update add.java

--- add.java
+++ add.java
@@ -1,10 +1,9 @@
-
 import java.util.*;
-public class add {
-
-    public static void main(String[] args) {
-        
-        int num1=4, num2=9, sum;
+public class add 
+{
+    public static void main(String[] args) 
+    {
+         int num1=4, num2=9, sum;
         sum = num1 + num2;
         System.out.println("Sum of these numbers: "+sum);
     }
